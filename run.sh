b01be5d6e4938021dca85f257294dc895422f438
李颂元 <lisongyuan@lisongyuans-MacBook-Air.local>
2014-10-08 21:35:25 +0800

    comment run.sh and modify .gitignore to adapt to my mac

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TFOLD="/tmp/$(basename $1).$$"
+TFOLD="/tmp/$(basename $1).$$" # basename to get the "base name", path reduced; $$ is the PID of this proceess
 mkdir -p $TFOLD
 
 LACKEY_LOG="$TFOLD/lackey".log
@@ -9,6 +9,7 @@ META_ROB_LOG="$TFOLD/meta_rob".log
 [ -e $LACKEY_LOG ] || mkfifo $LACKEY_LOG
 [ -e $META_ROB_LOG ] || mkfifo $META_ROB_LOG
 
+# $@ will expand to all the arguments of this bash script
 valgrind --log-file=$LACKEY_LOG --tool=lackey --trace-mem=yes --trace-superblocks=yes $@ &
 
 #-c number of cores; -s merged/minimum code block size; -d reorder-buffer depth

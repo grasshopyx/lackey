7f80f91c48e3d6f5cb9d52dcc27e855f59d85478
Lei Zungjun <grasshopyx@gmail.com>
2014-12-02 00:20:29 +0800

    This is an important commit. I fix the SB bug of exe_blk.lua. The original version miss the last SB.

--- exe_blk.lua
+++ exe_blk.lua
@@ -12,76 +12,97 @@ local miss_delay = 1
 
 function exe_blocks(core_num, rob_exe_log, miss_log)
 
-   local clkcount = 0
-   local addr, current_core = 0, 1
-   local icount_sb = 0
-   local accesstype, daddr, dsize
-   local misscnt_sb = 0
+    local clkcount = 0
+    local addr, current_core = 0, 1     -- current_core is the current core id (cid in meta_rob.lua)
+    local icount_sb = 0
+    local accesstype, daddr, dsize
+    local misscnt_sb = 0
 
-   -- #core = #miss_log
-   local core = {}
-   for i=1, core_num do
-      core[i] = {icount = 0, delay_count = 0, clk_pend = 0}
-   end
+    -- #core = #miss_log
+    local core = {}
+    for i=1, core_num do
+        core[i] = {icount = 0, delay_count = 0, clk_pend = 0}
+    end
 
-   for line in rob_exe_log:lines() do
-      if line:sub(1,2) ~= "#" then	 
-	 if line:sub(1,5) == "ISSUE" then
-	    -- a line of blocks get issued
-	    local max_clk = 0
-	    -- io.write("EXE ")
-	    for _, c in ipairs(core) do
-	       if max_clk < c.clk_pend then max_clk = c.clk_pend end
-	       -- io.write(string.format("%d ", c.clk_pend))
-	       c.clk_pend = 0
-	    end
-	    clkcount = clkcount + max_clk
-	    -- print(' CLK', max_clk)
+    -- local first_flag=true
 
-	 elseif line:sub(1,2) == "SB" then
-	    -- summarize the previous SB 1st
-	    local c = core[tonumber(current_core)]
-	    c.icount = c.icount + icount_sb
-	    c.delay_count = c.delay_count + misscnt_sb * miss_delay
-	    c.clk_pend = c.clk_pend + icount_sb + misscnt_sb * miss_delay
+    for line in rob_exe_log:lines() do
+        if line:sub(1,1) ~= "#" then	 
+            if line:sub(1,5) == "ISSUE" then
+                -- a line of blocks get issued
+                local max_clk = 0
+                -- io.write("EXE ")
 
-	    -- 
-	    addr, current_core, _icount = string.match(line:sub(4), "(%x+) (%d+) (%d+)")
-	    icount_sb = tonumber(_icount)
-	 elseif miss_log and #miss_log > 0 then
-	    -- now it is a memory reference, let's check the miss log
-	    -- and see how much latency it causes
-	    
-	    local mlog = miss_log[tonumber(current_core)]
-	    local miss_record = mlog:read("*line")
-	    if not miss_record then break end
-	    misscnt_sb = misscnt_sb + tonumber(miss_record:sub(6))
-	 end
-      end
-      
-   end				-- for exe in rob_exe_log.lines()
+                -- if first_flag ~= true then
+                    -- io.stderr:write("ISSUE", line:sub(6),":\t")
+                    -- for _, c in ipairs(core) do
+                    --     io.stderr:write(c.clk_pend, " ")
+                    -- end
+                    -- io.stderr:write("\n")
+                -- else
+                --     first_flag = false
+                -- end
 
-   local icount, delaycount = 0, 0
+                for _, c in ipairs(core) do
+                    if max_clk < c.clk_pend then max_clk = c.clk_pend end
+                    -- io.write(string.format("%d ", c.clk_pend))
+                    c.clk_pend = 0
+                end
+                clkcount = clkcount + max_clk
+                -- print(' CLK', max_clk)
 
-   for k, v in pairs(core) do
-      icount = icount + v.icount
-      delaycount = delaycount + v.delay_count
-   end
-   print(string.format("executed %d insts in %d clks: CPI=", icount, clkcount), clkcount/icount)   
+            elseif line:sub(1,2) == "SB" then
+                addr, current_core, _icount = string.match(line:sub(4), "(%x+) (%d+) (%d+)")
+                icount_sb = tonumber(_icount)
+
+                -- io.stderr:write("SB: ", addr, " " , current_core, " ", icount_sb,"\n")
+
+                -- summarize the previous SB 1st  -- it's wrong. it will miss the last SB
+                local c = core[tonumber(current_core)]
+                c.icount = c.icount + icount_sb
+                c.delay_count = c.delay_count + misscnt_sb * miss_delay     -- unused
+                c.clk_pend = c.clk_pend + icount_sb + misscnt_sb * miss_delay
+
+                -- 
+
+            elseif miss_log and #miss_log > 0 then
+                -- now it is a memory reference, let's check the miss log
+                -- and see how much latency it causes
+
+                local mlog = miss_log[tonumber(current_core)]
+                local miss_record = mlog:read("*line")
+                if not miss_record then break end
+                misscnt_sb = misscnt_sb + tonumber(miss_record:sub(6))
+            end
+            -- else
+            --     io.stderr:write('Yes!')
+        end
+
+    end				-- for exe in rob_exe_log.lines()
+
+    -- io.stderr:write('TEST STDERR')
+
+    local icount, delaycount = 0, 0
+
+    for k, v in pairs(core) do
+        icount = icount + v.icount
+        delaycount = delaycount + v.delay_count
+    end
+    print(string.format("executed %d insts in %d clks: CPI=", icount, clkcount), clkcount/icount)   
 
 end
 
 
 
 function open_traces(sched, ...)
-   local _sched = assert(io.open(sched, "r"))
-   
-   local _mref = {}
-   for i, v in ipairs{...} do
-      _mref[i] = assert(io.open(v, "r"))
-   end
+    local _sched = assert(io.open(sched, "r"))
 
-   return _sched, _mref
+    local _mref = {}
+    for i, v in ipairs{...} do
+        _mref[i] = assert(io.open(v, "r"))
+    end
+
+    return _sched, _mref
 end
 
 -- clk_add_delay(4, open_traces("./test/date_rob.log", "./test/cpu1.dinero", "./test/cpu2.dinero", "./test/cpu3.dinero", "./test/cpu4.dinero"))
--- meta_rob.lua
+++ meta_rob.lua
@@ -8,7 +8,7 @@
 local List = require "list"
 
 function logd(...)
-   print(...)
+    print(...)
 end
 
 -- the parameters that affects the parallelism 
@@ -21,29 +21,30 @@ local quit_at = 300000
 local reg_sync_delay = 4
 
 for i, v in ipairs(arg) do
-   --print(type(v))
-   if (v:sub(1,2) == "-c") then
-      --print("core number:")
-      core_num = tonumber(v:sub(3))
-   -- elseif (v:sub(1,2) == "-w") then
-   --    --print("ROB width:")
-   --    rob_w = tonumber(v:sub(3))
-   elseif (v:sub(1,2) == "-d") then
-      --print("ROB depth:")
-      rob_d = tonumber(v:sub(3))
-   elseif (v:sub(1,2) == "-s") then
-      --print("minimum superblock size:")
-      sb_size = tonumber(v:sub(3))
-   elseif (v:sub(1,2) == "-q") then
-      --print("minimum superblock size:")
-      quit_at = tonumber(v:sub(3))
-   elseif (v:sub(1,2) == "-mg") then	-- I think it should be v:sub(1,3) here
-      --print("minimum superblock size:")
-      sb_merge = true
-   end
+    --print(type(v))
+    if (v:sub(1,2) == "-c") then
+        --print("core number:")
+        core_num = tonumber(v:sub(3))
+        -- elseif (v:sub(1,2) == "-w") then
+        --    --print("ROB width:")
+        --    rob_w = tonumber(v:sub(3))
+    elseif (v:sub(1,2) == "-d") then
+        --print("ROB depth:")
+        rob_d = tonumber(v:sub(3))
+    elseif (v:sub(1,2) == "-s") then
+        --print("minimum superblock size:")
+        sb_size = tonumber(v:sub(3))
+    elseif (v:sub(1,2) == "-q") then
+        --print("minimum superblock size:")
+        quit_at = tonumber(v:sub(3))
+    elseif (v:sub(1,2) == "-mg") then	-- I think it should be v:sub(1,3) here
+        --print("minimum superblock size:")
+        sb_merge = true
+    end
 end
 
 -- collection of all the buffered sb's, key is the addr, val is the sb
+-- not all the sb, but buffered
 local sbs = {}
 local sbs_run = {}
 
@@ -51,13 +52,13 @@ local sbs_run = {}
 local rob = {}
 
 function init_rob(rob, MAX, WIDTH)
-   -- the rob.buf is a list of list, as each level of the rob shall
-   -- contain several sb's that with the same depth
-   -- E.g. with MAX=3 and WIDTH=2, it looks like
-   -- {{l00,l01},{l10,l11},{l20,l21}}
-   rob.buf = List.new()
-   rob.MAX = MAX
-   rob.WIDTH = WIDTH
+    -- the rob.buf is a list of list, as each level of the rob shall
+    -- contain several sb's that with the same depth
+    -- E.g. with MAX=3 and WIDTH=2, it looks like
+    -- {{l00,l01},{l10,l11},{l20,l21}}
+    rob.buf = List.new()
+    rob.MAX = MAX
+    rob.WIDTH = WIDTH
 end
 
 -- to record which SB writes to a specific memory address
@@ -74,7 +75,7 @@ local reg_input = {}
 
 local sb_addr = 0
 -- the SB on which the current sb depends
-local deps = {}
+local deps = {}		-- it is a subset of sbs
 local sb_weight = 0
 
 -- current SB
@@ -82,231 +83,251 @@ local reg_out_offset = {}
 local reg_in_offset = {}
 local reg_io = {}
 
-local blk_seq = 0
+local blk_seq = 0   -- unused
 
 -- we are entering a new superblock
 function start_sb(addr)
-   -- print("SB "..addr)
-   sb_addr = addr
+    -- print("SB "..addr)
+    sb_addr = addr
 end
 
 -- place the superblock in the rob
 function place_sb(rob, sb)
 
-   -- the sb should be placed after all of its depending sb's
-   local buf = rob.buf
-   -- d is the depth, i.e. in which level/line of the rob the sb should be put
-   local d = buf.first
-   local i = 0
-   for k, v in pairs(deps) do
-      i = i + 1
-      if d <= v.d then d = v.d end
-      -- logd(k, v.d)
-   end
+    -- the sb should be placed after all of its depending sb's
+    local buf = rob.buf
+    -- d is the depth, i.e. in which level/line of the rob the sb should be put
+    local d = buf.first
+    local i = 0
+    for k, v in pairs(deps) do
+        i = i + 1
+        -- logd(sb.addr, d)
+        if d <= v.d then d = v.d end
+        -- logd(k, v.d)
+        -- logd(sb.addr, d)
+    end
 
-   -- look for a non-full line which can hold the sb
-   found_slot = false
-   local l
-   for i=d+1, buf.last do
-      l = buf[i]
-      -- if #l < rob.WIDTH then 
-      if List.size(l) < rob.WIDTH then
-	 found_slot = true 
-	 d = i
-	 break
-      end
-   end
+    -- look for a non-full line which can hold the sb
+    found_slot = false
+    local l
+    for i=d+1, buf.last do
+        l = buf[i]
+        -- if #l < rob.WIDTH then 
+        if List.size(l) < rob.WIDTH then
+            found_slot = true 
+            d = i
+            break
+        end
+    end
 
-   if not found_slot then
-      List.pushright(buf, List.new())
-      --List.pushright(buf, {})
-      d = buf.last
-      l = buf[d]
-   end
+    if not found_slot then
+        List.pushright(buf, List.new())
+        --List.pushright(buf, {})
+        d = buf.last
+        l = buf[d]
+    end
 
-   -- place the sb in the proper level of the rob
-   sb['d'] = d 
-   -- l[#l + 1] = sb		-- the line is a List
-   List.pushright(l, sb)
-   logd('place:', sb.addr, d)
+    -- place the sb in the proper level of the rob
+    sb['d'] = d 
+    -- l[#l + 1] = sb		-- the line is a List
+    List.pushright(l, sb)
+    -- logd('place:', sb.addr, d)
 
-end				-- function place_sb(rob, sb)
+end				
+-- function place_sb(rob, sb)
 
 -- issue a line of sb's from the rob when necessary
 function issue_sb(rob)
-   local buf = rob.buf
-   -- if List.size(buf) > rob.MAX then
-   while List.size(buf) > rob.MAX do
-      local l = List.popleft(buf)
-      local w_sum = 0
-      local w_max = 0
-      local width = 0
+    local buf = rob.buf
+    -- if List.size(buf) > rob.MAX then
+    -- count=0
+    -- I confirmed that this while will do once at most.
+    while List.size(buf) > rob.MAX do
 
-      -- TODO add a switch verbose or terse
-      print('ISSUE', List.size(l))
-      
-      local cid = 1
-      while List.size(l) > 0 do
-	 v = List.popleft(l)
-	 --for k, v in ipairs(l) do
-	 width = width + 1
-	 w_sum = w_sum + v.w
-	 if w_max < 0 + v.w then
-	    w_max = 0 + v.w
-	 end
+        -- count=count+1
+        -- logd("count=",count)
 
-	 print(string.format('SB %s %d %d', v.addr, cid, v.w))
-	 cid = cid + 1
+        local l = List.popleft(buf)
+        local w_sum = 0
+        local w_max = 0     -- unused
+        local width = 0
 
-	 for _, mem_rw in ipairs(v.mem_access) do
-	    print(string.format('MEM %d %x', mem_rw.type, mem_rw.addr))
-	 end	 
+        -- TODO add a switch verbose or terse
+        print('ISSUE', List.size(l))
 
-	 sbs_run[v.addr] = sbs[v.addr]
-	 sbs[v.addr] = nil
-      end      
+        local cid = 1
+        while List.size(l) > 0 do
+            v = List.popleft(l)
+            --for k, v in ipairs(l) do
+            width = width + 1
+            w_sum = w_sum + v.w
+            if w_max < 0 + v.w then
+                w_max = 0 + v.w
+            end
 
-      -- TODO add a switch verbose or terse
-      -- logd(Core.clocks, w_sum, w_max, width, w_sum/w_max)
-   end
+            print(string.format('SB %s %d %d', v.addr, cid, v.w))
+            cid = cid + 1
+
+            for _, mem_rw in ipairs(v.mem_access) do
+                print(string.format('MEM %d %x', mem_rw.type, mem_rw.addr))
+            end	 
+
+            sbs_run[v.addr] = sbs[v.addr]
+            sbs[v.addr] = nil
+        end      
+
+        -- TODO add a switch verbose or terse
+        -- logd(Core.clocks, w_sum, w_max, width, w_sum/w_max)
+    end
 end
 
 
 -- the current superblock ends, we'll analyze it here
 function end_sb()
-   -- build the superblock
-   local sb = {}
+    -- build the superblock
+    local sb = {}
 
-   sb.seq = blk_seq
-   blk_seq = blk_seq + 1
+    sb.seq = blk_seq
+    blk_seq = blk_seq + 1
 
-   sb['addr'] = sb_addr
-   sb['w'] = sb_weight
-   sb['deps'] = deps
-   sb['mem_access'] = mem_access
+    sb['addr'] = sb_addr
+    sb['w'] = sb_weight
+    sb['deps'] = deps
+    sb['mem_access'] = mem_access
 
-   local dep_mem_cnt, dep_reg_cnt = 0, 0
-   for k, v in pairs(mem_input) do
-      dep_mem_cnt = dep_mem_cnt + v
-   end
-   for k, v in pairs(reg_input) do
-      dep_reg_cnt = dep_reg_cnt + v
-   end   
+    local dep_mem_cnt, dep_reg_cnt = 0, 0
+    for k, v in pairs(mem_input) do
+        dep_mem_cnt = dep_mem_cnt + v
+    end
+    for k, v in pairs(reg_input) do
+        dep_reg_cnt = dep_reg_cnt + v
+    end   
 
-   sb.dep_mem_cnt = dep_mem_cnt
-   sb.dep_reg_cnt = dep_reg_cnt
+    sb.dep_mem_cnt = dep_mem_cnt
+    sb.dep_reg_cnt = dep_reg_cnt
 
-   sb.reg_out_offset = reg_out_offset
-   sb.reg_in_offset = reg_in_offset
-   sb.reg_io = reg_io
+    sb.reg_out_offset = reg_out_offset
+    sb.reg_in_offset = reg_in_offset
+    sb.reg_io = reg_io
 
-   sbs[sb_addr] = sb
-   -- io.write(sb_addr.."<=")
-   -- for k, v in pairs(deps) do
-   --    io.write(k.." ")
-   -- end
-   -- print(' M:'..dep_mem_cnt..' R:'..dep_reg_cnt)
-   place_sb(rob, sb)
-   issue_sb(rob)
+    sbs[sb_addr] = sb
+    -- io.write(sb_addr.."<=")
+    -- for k, v in pairs(deps) do
+    --    io.write(k.." ")
+    -- end
+    -- print(' M:'..dep_mem_cnt..' R:'..dep_reg_cnt)
+    place_sb(rob, sb)
+    issue_sb(rob)
 
-   -- FIXME do this in the init_sb()
-   deps = {}
-   mem_input = {}
-   mem_access = {}
-   reg_input = {}
-   reg_out_offset = {}
-   reg_in_offset = {}
-   reg_io = {}
+    -- FIXME do this in the init_sb()
+    deps = {}
+    mem_input = {}
+    mem_access = {}
+    reg_input = {}
+    reg_out_offset = {}
+    reg_in_offset = {}
+    reg_io = {}
 
-end				-- end_sb()
+end				
+-- end_sb()
 
 -- the table deps is a set, we use addr as key, so searching it is
 -- efficient
 function add_depended(addr)
-   deps[addr] = sbs[addr]
-   -- print('add_depended:', addr)
+    deps[addr] = sbs[addr]
+    -- print('add_depended:', addr)
 end
 
 function set_sb_weight(w)
-   sb_weight = w
+    sb_weight = w
 end
 
 function parse_lackey_log(sb_size, sb_merge)	-- sb_merge is unused
-   local i = 0
-   local weight_accu = 0
-   local first_sb_flag=true
-   for line in io.lines() do
-      if line:sub(1,2) ~= '==' then
-	 i = i + 1
-	 local k = line:sub(1,2)
-	 if k == 'SB' then
-		 if first_sb_flag then
-			 start_sb(line:sub(4))
-			 first_sb_flag=false
-		 end
-	    -- if not sb_merge or
-	    if weight_accu >= sb_size then
-	       set_sb_weight(weight_accu)
-	       end_sb()
-	       start_sb(line:sub(4))	       
-	       weight_accu = 0
-	    end
-	 -- elseif k == 'I ' then	    
-	 elseif k == ' S' then
-	    -- local d_addr = tonumber(line:sub(4,11), 16)	-- why 11?
-	    local d_addr, _ = string.match(line:sub(4), "(%w+),(%d+)")	-- I think it's unnecessary to use number type
-	    d_addr=tonumber(d_addr,16)
+    local i = 0
+    local weight_accu = 0
+    local first_sb_flag=true
+    for line in io.lines() do
+        if line:sub(1,2) ~= '==' then
+            i = i + 1
+            local k = line:sub(1,2)
+            if k == 'SB' then
+                if first_sb_flag then
+                    start_sb(line:sub(4))
+                    first_sb_flag=false
+                end
+                -- if not sb_merge or
+                if weight_accu >= sb_size then
+                    set_sb_weight(weight_accu)
+                end_sb()
+                start_sb(line:sub(4))	       
+                weight_accu = 0
+            end
+            -- elseif k == 'I ' then	    
+        elseif k == ' S' then
+            -- local d_addr = tonumber(line:sub(4,11), 16)	-- why 11?
+            local d_addr, _ = string.match(line:sub(4), "(%w+),(%d+)")	-- I think it's unnecessary to use number type
+            d_addr=tonumber(d_addr,16)
 
-	    mem_writer[d_addr] = sb_addr
-	    mem_access[#mem_access + 1] = {type=1, addr=d_addr}
-	    -- logd("S", string.format("%x",d_addr))
-	 elseif k == ' L' then
-	    -- local d_addr = tonumber(line:sub(4,11), 16)
-	    local d_addr, mem_size = string.match(line:sub(4), "(%w+),(%d+)")	-- I think it's unnecessary to use number type
-	    d_addr=tonumber(d_addr,16)
-	    -- logd(line)
-	    -- logd(d_addr,mem_size)
-	    -- logd("L", string.format("%x",d_addr))
-	    mem_access[#mem_access + 1] = {type=0, addr=d_addr}
+            -- if mem_writer[d_addr] ~= nil then
+            -- logd(string.format("%x",d_addr),"origin:",mem_writer[d_addr])
+            -- end
+            -- Let there be two SBs, A and B, which are independent. Both A and B writes to d_addr. 
+            -- Suppose B is subsequent to A in the trace. B can be scheduled before A.
+            -- I think it is necessary to distinguish the two writes to d_addr.
+            mem_writer[d_addr] = sb_addr
+            -- if mem_writer[d_addr] ~= nil then
+            -- logd(string.format("%x",d_addr),"new:", mem_writer[d_addr])
+            -- end
+            mem_access[#mem_access + 1] = {type=1, addr=d_addr}
+            -- logd("S", string.format("%x",d_addr))
+        elseif k == ' L' then
+            -- local d_addr = tonumber(line:sub(4,11), 16)
+            local d_addr, mem_size = string.match(line:sub(4), "(%w+),(%d+)")	-- I think it's unnecessary to use number type
+            d_addr=tonumber(d_addr,16)
+            -- logd(line)
+            -- logd(d_addr,mem_size)
+            -- logd("L", string.format("%x",d_addr))
+            mem_access[#mem_access + 1] = {type=0, addr=d_addr}
 
-	    local dep = mem_writer[d_addr]
-	    if dep and dep ~= sb_addr then 
-	       -- io.write("L "..line:sub(4,11).." ")
-	       -- add_depended(dep) 
-	       -- mem_input[d_addr] = tonumber(line:sub(14))	-- why 13?
-	       mem_input[d_addr] = tonumber(mem_size)
-	       -- logd("L",mem_input[d_addr])
-	    end
-	 elseif k == ' P' then
-	    local reg_o, offset_sb = string.match(line:sub(4), "(%d+) (%d+)")
-	    reg_writer[tonumber(reg_o)] = sb_addr
-	    -- reg_writer_seq[tonumber(reg_o)] = blk_seq
-	    reg_out_offset[reg_o] = offset_sb
-	    reg_io[#reg_io + 1] = {io='o', reg=reg_o}
-	    -- logd("P", sb_addr, reg_o, offset_sb, reg_out_offset)
-	 elseif k == ' G' then
-	    reg_i, offset_sb = string.match(line:sub(4), "(%d+) (%d+)")
-	    local d_addr = tonumber(reg_i)
-	    local dep = reg_writer[d_addr]
-	    -- if dep and dep ~= sb_addr and blk_seq ~= reg_writer_seq[d_addr] then
-	    if dep and dep ~= sb_addr then 
-	       -- io.write("G "..line:sub(4).." ")
-	       add_depended(dep) 	-- what's this
-	       reg_input[d_addr] = 1
-	       reg_in_offset[reg_i] = offset_sb
-	       reg_io[#reg_io + 1] = {io='i', reg=reg_i, dep=dep}	-- what's this
-	    end
-	    -- logd("G", sb_addr, reg_i, offset_sb, dep)
-	 -- elseif k == ' D' then
-	 --    add_depended(line:sub(4))
-	 elseif k == ' W' then
-	    weight_accu = weight_accu + tonumber(line:sub(4))
-	 end
-      end
-   end
-   -- TODO add a switch verbose or terse
-   -- logd(i)
-end				--  function parse_lackey_log()
+            local dep = mem_writer[d_addr]
+            if dep and dep ~= sb_addr then 
+                -- io.write("L "..line:sub(4,11).." ")
+                -- add_depended(dep) 
+                -- mem_input[d_addr] = tonumber(line:sub(14))	-- why 13?
+                mem_input[d_addr] = tonumber(mem_size)
+                -- logd("L",mem_input[d_addr])
+            end
+        elseif k == ' P' then
+            local reg_o, offset_sb = string.match(line:sub(4), "(%d+) (%d+)")
+            reg_writer[tonumber(reg_o)] = sb_addr
+            -- reg_writer_seq[tonumber(reg_o)] = blk_seq
+            reg_out_offset[reg_o] = offset_sb
+            reg_io[#reg_io + 1] = {io='o', reg=reg_o}
+            -- logd("P", sb_addr, reg_o, offset_sb, reg_out_offset)
+        elseif k == ' G' then
+            reg_i, offset_sb = string.match(line:sub(4), "(%d+) (%d+)")
+            local d_addr = tonumber(reg_i)
+            local dep = reg_writer[d_addr]
+            -- if dep and dep ~= sb_addr and blk_seq ~= reg_writer_seq[d_addr] then
+            if dep and dep ~= sb_addr then 
+                -- io.write("G "..line:sub(4).." ")
+                add_depended(dep) 	-- add dep to the deps
+                reg_input[d_addr] = 1
+                reg_in_offset[reg_i] = offset_sb
+                reg_io[#reg_io + 1] = {io='i', reg=reg_i, dep=dep}	-- the dep is an address
+            end
+            -- logd("G", sb_addr, reg_i, offset_sb, dep)
+            -- elseif k == ' D' then
+            --    add_depended(line:sub(4))
+        elseif k == ' W' then
+            weight_accu = weight_accu + tonumber(line:sub(4))
+        end
+    end
+end
+-- TODO add a switch verbose or terse
+-- logd(i)
+end			
+--  function parse_lackey_log()
 
 rob_w = core_num
 init_rob(rob, rob_d, rob_w)
